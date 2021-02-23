75f8ad0a461df1b12297f4c8a0188040dfd06341
David Levai <david@davidlevai.com>
2021-02-23 16:31:50 +0100

    Fix casing discrepancies.

--- pages/content/[slug].tsx
+++ pages/content/[slug].tsx
@@ -2,7 +2,7 @@ import hydrate from 'next-mdx-remote/hydrate';
 
 import { getFiles, getFileBySlug } from '../../lib/mdx';
 import BlogLayout from '../../layouts/blog';
-import MDXComponents from '../../components/MDXComponents';
+import MDXComponents from '../../components/mdx-components';
 
 export default function Blog({ mdxSource, frontMatter }) {
   const content = hydrate(mdxSource, {
