4f54476203c6f502947c07b25e7db8f9845c6a31
David Levai <david@davidlevai.com>
2021-02-23 17:04:04 +0100

    fix css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import '../styles/globals.css';
 import { useAnalytics } from '../lib/analytics';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
