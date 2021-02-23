551bd7c271e819645a8289fe71ac0dc9d3d1f69d
David Levai <david@davidlevai.com>
2021-02-23 17:01:52 +0100

    removing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import '../styles/globals.css';
 import { useAnalytics } from '../lib/analytics';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
--- pages/content/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Content({ mdxSource, frontMatter }) {
-  return <p>WIP</p>;
-}
