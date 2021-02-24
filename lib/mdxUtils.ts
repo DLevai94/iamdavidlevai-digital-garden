97e05830add3f931d01eb6994651a7c1bf214b70
David Levai <david@davidlevai.com>
2021-02-24 18:36:24 +0100

    rehype magic

--- lib/mdxUtils.ts
+++ lib/mdxUtils.ts
@@ -1,8 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import mdxPrism from 'mdx-prism';
-
-export const mdxPrismPlugin = mdxPrism;
 
 // POSTS_PATH is useful when you want to get the path to a specific file
 export const POSTS_PATH = path.join(process.cwd(), 'data');
