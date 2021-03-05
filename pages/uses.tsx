4d583cc8e764af529b4158c0d7e087280352b4a6
David Levai <david@davidlevai.com>
2021-03-05 09:00:31 +0100

    yarn fix

--- pages/uses.tsx
+++ pages/uses.tsx
@@ -1,5 +1,11 @@
 const UsesPage = () => {
-  return <div>WIP</div>;
+  return (
+    <div>
+      <a href="https://usefathom.com/ref/F6KQFM" rel="noreferrer noopener">
+        Fathom Analytics
+      </a>
+    </div>
+  );
 };
 
 export default UsesPage;
